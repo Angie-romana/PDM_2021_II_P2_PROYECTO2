67cdb127dc33df67bbe971923d7808a62128a262
Mitsuki <angienicoll7@gmail.com>
2021-07-12 14:40:30 -0600

    Initial commit

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_proyecto2/GameActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_p2_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
